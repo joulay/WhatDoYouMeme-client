0d1ef5f88d10ef1d8377ff7b82e8818491182892
Julie K <julieskim160@gmail.com>
2018-05-29 11:23:48 -0700

    question reducer

---
+++ src/reducers/question.js
@@ -0,0 +1,28 @@
+import * as actions from '../actions/question';
+
+const initialState = {
+	question: [],
+	loading: false,
+	error: null
+};
+
+export default function questionReducer(state = initialState, action) {
+	if (action.type === actions.FETCH_QUESTION_REQUEST) {
+		return Object.assign({}, state, {
+			loading: true
+		});
+    }
+	if (action.type === actions.FETCH_QUESTION_SUCCESS) {
+		return Object.assign({}, state, {
+			loading: false,
+			question: action.question
+		});
+    }
+    if (action.type === actions.FETCH_QUESTION_ERROR) {
+		return Object.assign({}, state, {
+			loading: false,
+			error: action.error
+		});
+    }
+    return state;     
+}
