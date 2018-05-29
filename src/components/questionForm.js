732838c3b2a7fe73aeeb483519ad22547918851f
Julie K <julieskim160@gmail.com>
2018-05-29 11:44:32 -0700

    start handling submit value for question

--- src/components/questionForm.js
+++ src/components/questionForm.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
 export default class QuestionForm extends React.Component {
+    onSubmit(values) {
+
+    }
+
     render() {
     return (
-        <div>
+        <div className="question-form">
             <div className='image'>
                 <img src={this.props.image} alt="meme"/>
             </div>
-            <form className="userInput">
+            <form 
+                onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}
+                className="form">
                 <input type="text" placeholder="name this meme"/>
                 <button type="submit">+</button>
             </form>
