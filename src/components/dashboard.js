a20e239b2f6612ade072435e4479893c3cfe2242
Jeff Vega <jeff@intellibliss.com>
2018-05-30 09:10:39 -0700

    img resizing

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -2,26 +2,32 @@ import React from 'react';
 import {connect} from 'react-redux';
 import requiresLogin from './requires-login';
 // import {fetchProtectedData} from '../actions/protected-data';
-import { fetchQuestion, fetchLoad } from '../actions/question';
+import {fetchQuestion, fetchLoad} from '../actions/question';
 import QuestionForm from './questionForm';
- 
+import './dashboard.css'
 export class Dashboard extends React.Component {
     constructor(props) {
         super(props)
-            this.state={
-                currentQuestion:0
-            }
-            this.loadNext=this.loadNext.bind(this) //take load next to place as same level as everything else
-    }
-    
-    componentDidMount() {
-        this.props.dispatch(fetchQuestion());
+        this.state = {
+            currentQuestion: 0
+        }
+        this.loadNext = this
+            .loadNext
+            .bind(this) //take load next to place as same level as everything else
     }
 
-    loadNext(){
-        this.props.dispatch(fetchLoad(this.props.questionArray[this.state.currentQuestion]));
+    componentDidMount() {
+        this
+            .props
+            .dispatch(fetchQuestion());
+    }
+
+    loadNext() {
+        this
+            .props
+            .dispatch(fetchLoad(this.props.questionArray[this.state.currentQuestion]));
         this.setState({
-            currentQuestion: this.state.currentQuestion+1
+            currentQuestion: this.state.currentQuestion + 1
         })
     }
 
@@ -35,18 +41,21 @@ export class Dashboard extends React.Component {
                 {/* <div className="dashboard-protected-data">
                     Protected data: {this.props.protectedData}
                 </div> */}
-                <QuestionForm proploadNext={this.loadNext} propQuestion={this.props.currentQuestion}/>
+                <div className="img-question">
+                <QuestionForm
+                    proploadNext={this.loadNext}
+                    propQuestion={this.props.currentQuestion}/>
+                    </div>
             </div>
-        );  
-    }   
+        );
+    }
 }
 
 const mapStateToProps = state => {
     const {currentUser} = state.auth;
     // console.log(state)
     return {
-        username: state.auth.currentUser.username,
-        name: `${currentUser.fullname}`,
+        username: state.auth.currentUser.username, name: `${currentUser.fullname}`,
         // protectedData: state.protectedData.data
         questionArray: state.auth.currentUser.question,
         currentQuestion: state.question.currentQuestion
