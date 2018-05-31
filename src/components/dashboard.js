06c7be7e1279609a785dfad80402e87fb5842655
Julie K <julieskim160@gmail.com>
2018-05-31 08:50:35 -0700

    score component into dashboard

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -4,6 +4,7 @@ import requiresLogin from './requires-login';
 // import {fetchProtectedData} from '../actions/protected-data';
 import {fetchQuestion, fetchLoad, checkAnswer} from '../actions/question';
 import QuestionForm from './questionForm';
+import Score from './score';
 import './dashboard.css'
 
 export class Dashboard extends React.Component {
@@ -16,7 +17,7 @@ export class Dashboard extends React.Component {
             this.loadNext=this.loadNext.bind(this) //take load next to place as same level as everything else
     }
     
-    async componentDidMount() { 
+    async componentDidMount() { //return loadNext, fixing prev bug
         await this.props.dispatch(fetchQuestion());
         await this.loadNext();
     }
@@ -38,8 +39,8 @@ export class Dashboard extends React.Component {
         //     this.setState({response: `Incorrect. It's name is ${dbAnswer}`})
         // }
         await this.props.dispatch(checkAnswer(input))
-        await this.props.dispatch(fetchQuestion());
-        // ******need to display information and set it into state
+        await this.props.dispatch(fetchQuestion()); //every time user responds, re fetchQuestions
+
         
     }
 
@@ -54,9 +55,10 @@ export class Dashboard extends React.Component {
                 <div className="img-question">
                 <QuestionForm onSubmit={(e)=>this.onSubmit(e)}
                     proploadNext={this.loadNext}
-                    propQuestion={this.props.currentQuestion}/>
+                    propQuestion={this.props.currentQuestion} />
                 </div>
                 <div className="response">{this.state.response}</div>
+                <Score />
             </div>
         );
     }
